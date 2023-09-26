6f25ce5d2c1178e0b66fbef5e0b504e1434d9a80
Dilip Kanogiya <dilipkanogiya@gmail.com>
2023-09-26 11:13:40 +0530

    Update script.js
    Gradient added

--- Dat 20 - Async programming/js/script.js
+++ Dat 20 - Async programming/js/script.js
@@ -1,16 +1,20 @@
 // https://x-colors.yurace.pro/api/random
 
-let url = `https://x-colors.yurace.pro/api/random`;
+let url = `https://x-colors.yurace.pro/api/random?number=20`;
 
-function updateColor(hexcode){
+function updateColor(color1,color2){
     let box = document.getElementById("box");
-    box.setAttribute('style', `background:${hexcode}`)
+    box.style.backgroundImage = `linear-gradient(180deg,${color1},${color2}`
 }
 
+// function updateColor(hexcode){
+//     let box = document.getElementById("box");
+//     box.setAttribute('style', `background:${hexcode}`)
+// }
 async function changeColor() {
     let data = await fetch(url);
     let res = await data.json();
     // console.log(res);
     // console.log(res.hex);
-    updateColor(res.hex);
+    updateColor(res[0].hex,res[1].hex);
 }
